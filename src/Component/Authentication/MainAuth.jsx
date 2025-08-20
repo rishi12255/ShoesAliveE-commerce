66b0566e1960317f713770c5f7aeacef6c39002e
Rishi Malla <rishimalla37@gmail.com>
2025-08-20 22:55:47 +0545

    Signup bg

--- src/Component/Authentication/MainAuth.jsx
+++ src/Component/Authentication/MainAuth.jsx
@@ -5,9 +5,19 @@ import Login from "./Login/Login";
 const MainAuth = () => {
   const prevLogin = localStorage.getItem("prevLogin");
   const [screen, setScreen] = useState(prevLogin ? false : true);
+
   return (
-    <div className="flex h-[100vh] justify-center items-center bg-[url('./many.jpg')] bg-no-repeat bg-cover">
-      <div className="w-80 border border-slate-400 rounded-xl shadow-lg shadow-black/50 bg-white/70 ">
+    <div className="relative h-[100vh] flex justify-center items-center">
+
+      <div
+        className="absolute inset-0 bg-[url('./mains.jpg')] bg-cover bg-center filter blur-sm"
+      ></div>
+
+      
+      <div className="absolute inset-0 bg-black/5"></div>
+
+
+      <div className="relative w-80 border border-slate-400 rounded-xl shadow-lg shadow-black/50 bg-white/70">
         {screen ? (
           <Signup setScreen={setScreen} />
         ) : (
